fb8bb4f1f602850caf85dedbf1a4e50ebb2ffc43
Frank Rodríguez López <fr20587@gmail.com>
2020-05-16 13:00:10 -0400

    Update 16/5/20

--- include/form.php
+++ include/form.php
@@ -27,8 +27,8 @@ require 'phpmailer/src/SMTP.php';
 $toemails = array();
 
 $toemails[] = array(
-				'email' => 'your-email@website.com', // Your Email Address
-				'name' => 'Your Name' // Your Name
+				'email' => 'FR20587@gmail.com', // Your Email Address
+				'name' => 'Frank Rodríguez López' // Your Name
 			);
 
 
@@ -37,8 +37,8 @@ $toemails[] = array(
 ---------------------------------------------------*/
 
 $fromemail = array(
-				'email' => 'no-reply@website.com', // Company's Email Address (preferably currently used Domain Name)
-				'name' => 'Company Name' // Company Name
+				'email' => 'athenda.cu@gmail.com', // Company's Email Address (preferably currently used Domain Name)
+				'name' => 'ATHENDAT' // Company Name
 			);
 
 
